e16dcfbf36c385f93d22f2d946bd388511677c8d
dan-tovytch <4464640@alunouninter.com>
2023-08-16 15:57:01 -0300

    Aritmeticos

--- src/Basic/fudamentos/Aritmeticos.java
+++ src/Basic/fudamentos/Aritmeticos.java
@@ -1,7 +1,29 @@
 package Basic.fudamentos;
 
-public class Aritimeticos {
+public class Aritmeticos {
     public static void main(String[] args) {
-        
+
+        System.out.println(2 + 3);
+
+        var x = 34.56;
+        double y = 2.2;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int a = 8;
+        int b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a / (double) b);
+        System.out.println(a / (float) b);
+
+        System.out.println(a % b);
+        System.out.println(8 % 3);
     }
 }
