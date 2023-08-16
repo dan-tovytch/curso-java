feb1680dd231129bd4149b6e51d364dc16d8a2f2
dan-tovytch <4464640@alunouninter.com>
2023-08-16 16:29:06 -0300

    Update desafioAritmeticos

--- src/Exercise/desafioAritmeticos.java
+++ src/Exercise/desafioAritmeticos.java
@@ -3,7 +3,20 @@ package Exercise;
 public class desafioAritmeticos {
     public static void main(String[] args) {
 
-        int a = 2 + 3 * 4;
-        int b = (int)Math.pow(a, 3);
+        double numA = Math.pow(6 * (3 + 2), 2);
+        double denA = 3 * 2;
+
+        double numB = (1 - 5) * (2 - 7);
+        double denB = 2;
+
+        double superiorA = numA / denA;
+        double superiorB = Math.pow(numB / denB, 2);
+
+        double superior = Math.pow(superiorA - superiorB, 3);
+        double inferior = Math.pow(10, 3);
+
+        double resultado = superior / inferior;
+
+        System.out.println("O resultado é " + resultado);
     }
 }
