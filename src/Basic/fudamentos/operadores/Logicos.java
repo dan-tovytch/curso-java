fe6f3f1fec45788d254581153d176eab751cebf9
dan-tovytch <4464640@alunouninter.com>
2023-08-17 16:21:24 -0300

    Update Logicos

--- src/Basic/fudamentos/operadores/Logicos.java
+++ src/Basic/fudamentos/operadores/Logicos.java
@@ -1,4 +1,37 @@
 package Basic.fudamentos.operadores;
 
 public class Logicos {
+    public static void main(String[] args) {
+
+        boolean codicao1 = true;
+        boolean codicao2 = 3 > 7;
+
+        System.out.println(codicao1 && !codicao2);
+        System.out.println(codicao1 || codicao2);
+        System.out.println(codicao1 ^ codicao2);
+        System.out.println(!!codicao1);
+        System.out.println(!codicao2);
+
+        System.out.println("Tabela verdade E (AND)");
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+        System.out.println("\nTabela verdade OU (OR)");
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+        System.out.println("\nTabela verdade OU Exclusivo (XOR)");
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ true);
+        System.out.println(false ^ false);
+
+        System.out.println("\nTabela verdade NOT");
+        System.out.println(!true);
+        System.out.println(!false);
+    }
 }
