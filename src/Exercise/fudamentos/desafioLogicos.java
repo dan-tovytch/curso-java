e4e27c4b37cb3e27fb42ee7d945899302affc392
dan-tovytch <4464640@alunouninter.com>
2023-08-20 20:47:36 -0300

    Update desafioLogicos

--- src/Exercise/fudamentos/desafioLogicos.java
+++ src/Exercise/fudamentos/desafioLogicos.java
@@ -4,16 +4,17 @@ public class desafioLogicos {
     public static void main(String[] args) {
 
         boolean trab1 = true;
-        boolean trab2 = true;
+        boolean trab2 = false;
 
         boolean tv30 = trab1 && trab2;
         boolean tv50 = trab1 ^ trab2;
 
-        boolean soverte = (trab1 && trab2) || (trab1 ^ trab2);
+        boolean soverte = trab1 || trab2;
 
         System.out.println("Televisão de 30 polegadas: " + tv30);
         System.out.println("Televisão de 50 polegadas: " + tv50);
         System.out.println("sorvete: " + soverte);
+        System.out.println("Saudável: " + !soverte);
 
     }
 }
