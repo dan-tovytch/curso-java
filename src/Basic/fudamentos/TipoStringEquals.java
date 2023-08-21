a87e680b5658d3abd3740e17ef2788ad49622a7f
dan-tovytch <4464640@alunouninter.com>
2023-08-21 16:17:03 -0300

    Update TipoStringEquals

--- src/Basic/fudamentos/TipoStringEquals.java
+++ src/Basic/fudamentos/TipoStringEquals.java
@@ -1,2 +1,21 @@
-package Basic.fudamentos;public class TipoStringEquals {
+package Basic.fudamentos;
+
+import java.util.Scanner;
+
+public class TipoStringEquals {
+    public static void main(String[] args) {
+        System.out.println("2" == "2");
+
+        String s1 = new String("2");
+        System.out.println("2" == s1);
+        System.out.println("2".equals(s1));
+
+        Scanner entrada = new Scanner(System.in);
+
+        String s2 = entrada.nextLine();
+        System.out.println("2" == s2.trim());
+        System.out.println("2".equals(s2.trim()));
+
+        entrada.close();
+    }
 }
