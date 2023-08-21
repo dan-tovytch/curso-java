055907c7926d6668f0c81ab2774112fa0513cf0a
dan-tovytch <4464640@alunouninter.com>
2023-08-21 16:09:45 -0300

    Update Ternario

--- src/Basic/fudamentos/operadores/Ternario.java
+++ src/Basic/fudamentos/operadores/Ternario.java
@@ -1,2 +1,19 @@
-package Basic.fudamentos.operadores;public class Ternario {
+package Basic.fudamentos.operadores;
+
+public class Ternario {
+    public static void main(String[] args) {
+        double media = 7.6;
+        String resultadoFinal = media >= 7.0 ? "Aprovado" : "Em recuperação." ;
+
+        System.out.println("O aluno está " + resultadoFinal);
+
+        double nota = 9.9;
+        boolean bomComportamento = true;
+        boolean passouPormedia = nota >= 7;
+        boolean temdesconto = bomComportamento && passouPormedia;
+        String resultado = temdesconto ? "sim" : "não";
+
+        System.out.printf("Tem desconto? %s", resultado);
+
+    }
 }
