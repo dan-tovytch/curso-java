4ce1fbd64a287a4eaf22ca6de5ee24d9a32a51f1
dan-tovytch <4464640@alunouninter.com>
2023-08-21 19:25:37 -0300

    Desafio converter celsius para fahrenheit

--- src/Exercise/fudamentos/desafioFinalFundamentos2.java
+++ src/Exercise/fudamentos/desafioFinalFundamentos2.java
@@ -1,2 +1,15 @@
-package Exercise.fudamentos;public class desafioFinalFundamentos2 {
+package Exercise.fudamentos;
+
+import javax.swing.*;
+
+public class desafioFinalFundamentos2 {
+    public static void main(String[] args) {
+        String celsiusInput = JOptionPane.showInputDialog("Digite a temperatura em celsius:");
+        double celsius = Double.parseDouble(celsiusInput);
+
+        // Realize os cálculos corretamente para converter de Fahrenheit para Celsius
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("A temperatura em Celsius é: " + fahrenheit + " ºF");
+    }
 }
