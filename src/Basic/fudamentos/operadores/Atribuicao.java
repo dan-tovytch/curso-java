618274c0537fc53de1c6387ef339f91215100975
dan-tovytch <4464640@alunouninter.com>
2023-08-21 15:45:07 -0300

    Update Atribuicao

--- src/Basic/fudamentos/operadores/Atribuicao.java
+++ src/Basic/fudamentos/operadores/Atribuicao.java
@@ -1,4 +1,20 @@
 package Basic.fudamentos.operadores;
 
 public class Atribuicao {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a;
+        int c = a + b;
+
+        c += b; // c = c + b
+        c -= a; // c = c - a
+        c *= b; // c = c * b
+        c /= a; // c = c / a
+
+        c++; // c = c + 1
+
+        System.out.println(c);
+        c %= 2; // c = c % 2; 0 ou 1
+        System.out.println(c);
+    }
 }
