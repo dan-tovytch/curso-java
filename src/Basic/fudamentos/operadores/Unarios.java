9a721c33806eb6195df5a127e2e1d8a5536a6ed2
dan-tovytch <4464640@alunouninter.com>
2023-08-21 15:55:05 -0300

    Update Unarios

--- src/Basic/fudamentos/operadores/Unarios.java
+++ src/Basic/fudamentos/operadores/Unarios.java
@@ -1,4 +1,24 @@
 package Basic.fudamentos.operadores;
 
 public class Unarios {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        a++; // a = a + 1
+        a--; // a = a - 1
+
+        ++b; // b = b + 1
+        --b; // b = b - 1
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("Mini desáfio...");
+        System.out.println(++a == b--);
+        System.out.println(a == b);
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
