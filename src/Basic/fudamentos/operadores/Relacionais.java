d089ee71d72a3bc9fcde67632c857d46e4db3b01
dan-tovytch <4464640@alunouninter.com>
2023-08-21 15:39:56 -0300

    Update Relacionais

--- src/Basic/fudamentos/operadores/Relacionais.java
+++ src/Basic/fudamentos/operadores/Relacionais.java
@@ -1,4 +1,22 @@
 package Basic.fudamentos.operadores;
 
 public class Relacionais {
+    public static void main(String[] args) {
+
+        int a = 97;
+        int b = 'a';
+
+        System.out.println(a == b);
+        System.out.println(3 > 4);
+        System.out.println(3 >= 3);
+        System.out.println(3 < 7);
+        System.out.println(30 <= 7);
+
+        double nota = 7.3;
+        boolean bomComportamento = false;
+        boolean passouPormedia = nota >= 7;
+        boolean temdesconto = bomComportamento && passouPormedia;
+
+        System.out.println("Tem desconto? " + temdesconto);
+    }
 }
