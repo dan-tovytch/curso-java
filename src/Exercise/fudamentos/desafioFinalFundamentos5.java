63c7e0a57d178499588d61bad529c85164228e15
dan-tovytch <4464640@alunouninter.com>
2023-08-21 23:09:15 -0300

    Update desafioFinalFundamentos5

--- src/Exercise/fudamentos/desafioFinalFundamentos5.java
+++ src/Exercise/fudamentos/desafioFinalFundamentos5.java
@@ -1,2 +1,17 @@
-package Exercise.fudamentos;public class desafioFinalFundamentos5 {
+package Exercise.fudamentos;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class desafioFinalFundamentos5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String baseTrianguloInput = JOptionPane.showInputDialog("Digite o valor da base do triângulo:");
+        double baseTriangulo = Double.parseDouble(baseTrianguloInput);
+        String alturaTrianguloInput = JOptionPane.showInputDialog("Digite o valor da altura do triângulo:");
+        double alturaTriangulo = Double.parseDouble(alturaTrianguloInput);
+
+        double area = (baseTriangulo * alturaTriangulo) / 2;
+        System.out.println("A área do triângulo é: " + area);
+    }
 }
