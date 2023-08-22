753b7b915045ee9fdd4350fbfc7c208e14a71897
dan-tovytch <4464640@alunouninter.com>
2023-08-21 22:41:58 -0300

    Update desafioFinalFundamentos3

--- src/Basic/fudamentos/desafioFinalFundamentos3.java
+++ src/Basic/fudamentos/desafioFinalFundamentos3.java
@@ -1,2 +1,18 @@
-package Basic.fudamentos;public class desafioFinalFundamentos3 {
+package Basic.fudamentos;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class desafioFinalFundamentos3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String alturaInput = JOptionPane.showInputDialog("Digite a sua altura:");
+        double altura = Double.parseDouble(alturaInput);
+        String pesoInput = JOptionPane.showInputDialog("Digite seu peso:");
+        double peso = Double.parseDouble(pesoInput);
+
+        double resultado = peso / (altura * altura);
+
+        System.out.println("Seu IMC é: " + resultado);
+    }
 }
