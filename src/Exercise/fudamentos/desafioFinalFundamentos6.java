d4a92ac3cd587e834a5a194f20093c17d18129cf
dan-tovytch <4464640@alunouninter.com>
2023-08-21 23:18:54 -0300

    Update ultimo desafioFinalFundamentos6

--- src/Exercise/fudamentos/desafioFinalFundamentos6.java
+++ src/Exercise/fudamentos/desafioFinalFundamentos6.java
@@ -1,2 +1,23 @@
-package Exercise.fudamentos;public class desafioFinalFundamentos6 {
+package Exercise.fudamentos;
+
+import java.util.Scanner;
+
+public class desafioFinalFundamentos6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o valor do A: ");
+        String valorA = entrada.next();
+        double a = Double.parseDouble(valorA);
+        System.out.print("Digite o valor do B: ");
+        String valorB = entrada.next();
+        double b = Double.parseDouble(valorB);
+        System.out.print("Digite o valor do C: ");
+        String valorC = entrada.next();
+        double c = Double.parseDouble(valorC);
+
+        double delta = Math.pow(b, 2) - 4 * a * c;
+
+        System.out.println("O valor do delta é: " + delta);
+    }
 }
