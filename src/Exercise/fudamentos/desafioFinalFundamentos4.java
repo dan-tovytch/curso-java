8e3dc3dfb1c8c7df425283505e7dc097596187c2
dan-tovytch <4464640@alunouninter.com>
2023-08-21 22:54:23 -0300

    Update desafioFinalFundamentos4

--- src/Exercise/fudamentos/desafioFinalFundamentos4.java
+++ src/Exercise/fudamentos/desafioFinalFundamentos4.java
@@ -1,2 +1,20 @@
-package Exercise.fudamentos;public class desafioFinalFundamentos4 {
+package Exercise.fudamentos;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class desafioFinalFundamentos4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o valor do quadrado: ");
+        String valorQuadrado = entrada.next();
+        double quadrado = Double.parseDouble(valorQuadrado);
+        double somaQuadrado = quadrado * quadrado;
+        double cubo = quadrado * quadrado * quadrado;
+
+        System.out.println("O valor do quadrado é: " + quadrado);
+        System.out.println("O valor do cubo é: " + cubo);
+
+    }
 }
