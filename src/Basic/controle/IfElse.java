ab8ccd977112060d79811a8cd5497fd91153cbce
dan-tovytch <4464640@alunouninter.com>
2023-08-23 14:31:07 -0300

    Update IfElse

--- src/Basic/controle/IfElse.java
+++ src/Basic/controle/IfElse.java
@@ -1,2 +1,17 @@
-package Basic.controle;public class IfElse {
+package Basic.controle;
+
+import javax.swing.*;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog("Informe o número");
+        int numero = Integer.parseInt(valor);
+
+        if (numero % 2 == 0) {
+            System.out.println("Número par!");
+        } else {
+            System.out.println("Número ímpar");
+        }
+    }
 }
