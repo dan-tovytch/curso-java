e5abb8e63950c1ec50978b882aeee4732c7742bc
dan-tovytch <4464640@alunouninter.com>
2023-08-23 14:25:07 -0300

    Update desafioIF

--- src/Exercise/controle/desafioIF.java
+++ src/Exercise/controle/desafioIF.java
@@ -1,2 +1,13 @@
-package Exercise.controle;public class desafioIF {
+package Exercise.controle;
+
+public class desafioIF {
+    public static void main(String[] args) {
+        double nota = 1.3;
+
+        // não usar ; em estrutura de controle (tem um exceção)
+        if (nota >= 9.0) {
+            System.out.println("Quadro de honra!");
+            System.out.println("Você é fera!!!");
+        }
+    }
 }
