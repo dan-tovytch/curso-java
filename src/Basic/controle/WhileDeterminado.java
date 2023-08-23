1d341d9c518c9858c380500f2180662614e3edd3
dan-tovytch <4464640@alunouninter.com>
2023-08-23 16:36:49 -0300

    Update WhileDeterminado

--- src/Basic/controle/WhileDeterminado.java
+++ src/Basic/controle/WhileDeterminado.java
@@ -1,2 +1,13 @@
-package Basic.controle;public class WhileDeterminado {
+package Basic.controle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 20) {
+            System.out.printf("i = %d\n", contador);
+            contador+= 2;
+        }
+    }
 }
