c0db29e7ae588264b9a88384d9c8b4e6ef6d2562
dan-tovytch <4464640@alunouninter.com>
2023-08-23 16:42:40 -0300

    Update For1

--- src/Basic/controle/For1.java
+++ src/Basic/controle/For1.java
@@ -1,2 +1,14 @@
-package Basic.controle;public class For1 {
+package Basic.controle;
+
+public class For1 {
+    public static void main(String[] args) {
+
+        for (int contador = 1; contador <= 10; contador ++) {
+            System.out.printf("i = %d\n", contador);
+        }
+        /* Laço infinito
+        * for(;;) {
+        * System.out.println("fim!")
+        * }*/
+    }
 }
