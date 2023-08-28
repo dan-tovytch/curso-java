b68fbce74a616e047bfb8e8f3ed59117a049718a
dan-tovytch <4464640@alunouninter.com>
2023-08-28 15:31:01 -0300

    Update whileinderteminado

--- src/Basic/controle/WhileInderteminado.java
+++ src/Basic/controle/WhileInderteminado.java
@@ -1,2 +1,19 @@
-package Basic.controle;public class WhileInderteminado {
+package Basic.controle;
+
+import java.util.Scanner;
+
+public class WhileInderteminado {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String valor = "";
+
+        while(!valor.equalsIgnoreCase("sair")) {
+            System.out.print("Você diz: ");
+            entrada.nextLine();
+            valor = entrada.nextLine();
+        }
+
+        entrada.close();
+    }
 }
