c6ed2d6c4d42eab7bfc8a0895341b3f3fe70ee91
dan-tovytch <4464640@alunouninter.com>
2023-09-04 14:41:32 -0300

    Update doWhile

--- src/Basic/controle/DoWhile.java
+++ src/Basic/controle/DoWhile.java
@@ -1,2 +1,20 @@
-package Basic.controle;public class DoWhile {
+package Basic.controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String texto = "";
+
+        do {
+            System.out.println("vc precisa falar as palavras mágicas...");
+            System.out.print("Quer sair? ");
+            texto = entrada.nextLine();
+        } while (!texto.equalsIgnoreCase("por favor"));
+
+        System.out.println("Obrigado!");
+        entrada.close();
+    }
 }
