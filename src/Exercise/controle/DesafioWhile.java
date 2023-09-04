31ea6a101e3b51ea4a8509857effadeab484678f
dan-tovytch <4464640@alunouninter.com>
2023-09-04 15:06:59 -0300

    Update DesafioWhile

--- src/Exercise/controle/DesafioWhile.java
+++ src/Exercise/controle/DesafioWhile.java
@@ -1,2 +1,29 @@
-package Exercise.controle;public class DesafioWhile {
+package Exercise.controle;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double total = 0;
+        double soma = 0;
+        double contador = 0;
+
+        do {
+            System.out.print("Digite a nota que deseja calcular: ");
+            total = entrada.nextDouble();
+            if ((total <= 10 && total >= 0)) {
+                System.out.println("Nota inválida");
+            } else {
+                soma += total;
+            } if (total != 0) {
+
+                contador++;
+            }
+        } while (total != -1);
+            System.out.println(contador);
+            System.out.println("A média das notas são de: " + soma / contador);
+        entrada.close();
+    }
 }
