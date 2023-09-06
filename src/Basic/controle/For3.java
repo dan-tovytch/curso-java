3680ffd66fc51e06719f4633b71e0b55d6fc9f9b
dan-tovytch <4464640@alunouninter.com>
2023-09-06 15:22:59 -0300

    Update for3

--- src/Basic/controle/For3.java
+++ src/Basic/controle/For3.java
@@ -1,2 +1,12 @@
-package Basic.controle;public class For3 {
+package Basic.controle;
+
+public class For3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("[%d \n %d]", i, j);
+            }
+            System.out.println();
+        }
+    }
 }
