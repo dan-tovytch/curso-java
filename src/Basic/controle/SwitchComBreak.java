0702a35cc3304e6aadf23f426d3ee538fbbb1069
dan-tovytch <4464640@alunouninter.com>
2023-09-06 16:49:38 -0300

    Update SwitchComBreak

--- src/Basic/controle/SwitchComBreak.java
+++ src/Basic/controle/SwitchComBreak.java
@@ -1,7 +1,35 @@
 package Basic.controle;
 
+import java.util.Scanner;
+
 public class SwitchComBreak {
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
+        String conceito = "";
+        System.out.print("Informe a nota: ");
+        int nota = entrada.nextInt();
+        switch (nota) {
+            case 10, 9:
+                conceito = "A";
+                break;
+            case 8, 7:
+                conceito = "B";
+                break;
+            case 6, 5:
+                conceito = "C";
+                break;
+            case 4, 3:
+                conceito = "D";
+                break;
+            case 2, 1, 0:
+                conceito = "E";
+                break;
+            default:
+                conceito = "Não encontrado";
+                break;
+        }
+
+        System.out.println("Conceito é " + conceito);
     }
 }
