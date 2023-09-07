a58407f2e458ea4a306ca2bdb98c7d9a95a03c9c
dan-tovytch <4464640@alunouninter.com>
2023-09-07 02:11:01 -0300

    Update DesafioFinal5

--- src/Exercise/controle/desafioFinalControle/DesafioFinal5.java
+++ src/Exercise/controle/desafioFinalControle/DesafioFinal5.java
@@ -1,2 +1,26 @@
-package Exercise.controle.desafioFinalControle;public class DesafioFinal5 {
+package Exercise.controle.desafioFinalControle;
+
+import java.util.Scanner;
+
+public class DesafioFinal5 {
+    public static void main(String[] args) {
+        int contadorDivisor = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+        int numero = entrada.nextInt();
+
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                contadorDivisor++;
+            }
+        }
+
+        switch (contadorDivisor) {
+            case 0:
+                System.out.println("O numero " + numero + " é um numero primo.");
+                break;
+            default:
+                System.out.println("O numero " + numero + "  não é um numero primo.");
+        }
+    }
 }
