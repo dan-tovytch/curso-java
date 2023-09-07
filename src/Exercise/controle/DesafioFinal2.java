f894785909df25d85c802172fc196ff7212de613
dan-tovytch <4464640@alunouninter.com>
2023-09-07 00:46:18 -0300

    Update DesafioFinal2

--- src/Exercise/controle/DesafioFinal2.java
+++ src/Exercise/controle/DesafioFinal2.java
@@ -1,2 +1,16 @@
-package Exercise.controle;public class DesafioFinal2 {
+package Exercise.controle;
+
+import java.util.Scanner;
+
+public class DesafioFinal2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite um ano: ");
+        int ano = entrada.nextInt();
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+            System.out.println("O ano é bissexto.");
+        } else {
+            System.out.println("O ano não é bissexto");
+        }
+    }
 }
