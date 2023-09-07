e20ae9a14915e4924da2ba64018abf27842b370f
dan-tovytch <4464640@alunouninter.com>
2023-09-07 02:45:33 -0300

    Update DesafioFinal9

--- src/Exercise/controle/desafioFinalControle/DesafioFinal9.java
+++ src/Exercise/controle/desafioFinalControle/DesafioFinal9.java
@@ -1,7 +1,25 @@
 package Exercise.controle.desafioFinalControle;
 
+import java.util.Scanner;
+
 public class DesafioFinal9 {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
+        int maiorValor = 0;
+        int contador = 0;
+
+        do {
+            System.out.print("\nDigite um número: ");
+            int valor = entrada.nextInt();
+
+            if (valor > maiorValor) {
+                maiorValor = valor;
+            }
+            contador++;
+        } while (contador != 10);
+        System.out.printf("O maior valor foi: " + maiorValor);
+
+        entrada.close();
     }
 }
