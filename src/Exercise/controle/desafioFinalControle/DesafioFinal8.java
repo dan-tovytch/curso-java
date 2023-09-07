bc06291fc5d86511e589314b502b34b8f507a87f
dan-tovytch <4464640@alunouninter.com>
2023-09-07 02:41:31 -0300

    Update DesafioFinal8

--- src/Exercise/controle/desafioFinalControle/DesafioFinal8.java
+++ src/Exercise/controle/desafioFinalControle/DesafioFinal8.java
@@ -1,2 +1,20 @@
-package Exercise.controle.desafioFinalControle;public class DesafioFinal8 {
+package Exercise.controle.desafioFinalControle;
+
+import java.util.Scanner;
+
+public class DesafioFinal8 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite a palavra: ");
+        String palavra = entrada.nextLine();
+
+        char letras[] = palavra.toCharArray();
+
+        for (int i = 0; i < letras.length; i++) {
+            System.out.println(letras[i]);
+        }
+
+        entrada.close();
+    }
 }
