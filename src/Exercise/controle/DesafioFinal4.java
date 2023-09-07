8791ba0ba581783e85e0ee14fefaf50211161e0b
dan-tovytch <4464640@alunouninter.com>
2023-09-07 02:06:17 -0300

    Update DesafioFinal4

--- src/Exercise/controle/DesafioFinal4.java
+++ src/Exercise/controle/DesafioFinal4.java
@@ -1,2 +1,26 @@
-package Exercise.controle;public class DesafioFinal4 {
+package Exercise.controle;
+
+import java.util.Scanner;
+
+public class DesafioFinal4 {
+    public static void main(String[] args) {
+        int contadorDivisor = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+        int numero = entrada.nextInt();
+
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                contadorDivisor++;
+            }
+        }
+        if (contadorDivisor == 0) {
+            System.out.println("\nO numero " + numero + " é primo.");
+        } else {
+            System.out.println("\nO numero " + numero + " não é primo.");
+        }
+
+
+        entrada.close();
+    }
 }
