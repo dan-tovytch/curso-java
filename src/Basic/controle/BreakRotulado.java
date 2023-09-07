bbdc006cb44fd8c0e222d86469eb0b7795b9f7bb
dan-tovytch <4464640@alunouninter.com>
2023-09-07 00:11:02 -0300

    Update BreakRotulado

--- src/Basic/controle/BreakRotulado.java
+++ src/Basic/controle/BreakRotulado.java
@@ -1,2 +1,19 @@
-package Basic.controle;public class BreakRotulado {
+package Basic.controle;
+
+public class BreakRotulado {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1) {
+                    break;
+                }
+                System.out.printf("[%d %d] ", i, j);
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Fim!");
+    }
 }
