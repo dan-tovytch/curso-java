63f400b6735ddf73ba693c142119aa1c6e67026e
dan-tovytch <4464640@alunouninter.com>
2023-09-07 00:01:11 -0300

    Update continue

--- src/Basic/controle/Continue.java
+++ src/Basic/controle/Continue.java
@@ -1,2 +1,16 @@
-package Basic.controle;public class Continue {
+package Basic.controle;
+
+public class Continue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        for (int i = 0; i <= 10; i++) {
+            if (i == 5) continue;
+            System.out.println(i);
+        }
+    }
 }
