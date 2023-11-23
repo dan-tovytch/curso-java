598de76b832afb3003bbaecbb745d80d1301f931
dan-tovytch <4464640@alunouninter.com>
2023-11-23 15:44:40 -0300

    Update

--- src/Basic/Classes/Produto.java
+++ src/Basic/Classes/Produto.java
@@ -6,4 +6,12 @@ public class Produto {
     double preco;
     double desconto;
 
+    double precoComDesconto() {
+        return preco * (1 - desconto);
+    }
+
+    double precoComDesconto(double descontoDoGerente) {
+        return preco * (1 - desconto + descontoDoGerente);
+    }
+
 }
--- src/Basic/Classes/ProdutoTeste.java
+++ src/Basic/Classes/ProdutoTeste.java
@@ -18,8 +18,8 @@ public class ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double precoFinal1 = p1.preco * (1 - p1.desconto);
-        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        double precoFinal1 = p1.precoComDesconto();
+        double precoFinal2 = p2.precoComDesconto(0.1);
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
         System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);
