54c2ac8ce5bd066bc3be41cf01024938fb9fb128
dan-tovytch <4464640@alunouninter.com>
2023-11-24 18:45:34 -0300

    Update ProdutoTeste

--- src/Basic/Classes/ProdutoTeste.java
+++ src/Basic/Classes/ProdutoTeste.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto();
-        p1.nome = "Notebook";
-        p1.preco = 4356.89;
-        p1.desconto = 0.25;
+        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
 
         var p2 = new Produto();
         p2.nome = "Caneta azul";
