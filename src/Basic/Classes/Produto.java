2d4f0c392cb12c09cfa27099fe03d2a67a314989
dan-tovytch <4464640@alunouninter.com>
2023-12-10 15:52:32 -0300

    Update Produto.java

--- src/Basic/Classes/Produto.java
+++ src/Basic/Classes/Produto.java
@@ -6,6 +6,16 @@ public class Produto {
     double preco;
     double desconto;
 
+    Produto() {
+
+    }
+
+    Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+        nome = nomeInicial;
+        preco = precoInicial;
+        desconto = descontoInicial;
+    }
+
     double precoComDesconto() {
         return preco * (1 - desconto);
     }
