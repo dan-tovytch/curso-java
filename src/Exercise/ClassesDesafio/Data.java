812e8a75a22dad4fd0a24c12c7713a75abb748d5
dan-tovytch <4464640@alunouninter.com>
2023-12-10 17:12:55 -0300

    Update

--- src/Exercise/ClassesDesafio/Data.java
+++ src/Exercise/ClassesDesafio/Data.java
@@ -1,10 +1,22 @@
 package Exercise.ClassesDesafio;
 
-public class DesafioClasseDatas2 {
+public class Data {
 
     int Mes;
     int Dia;
     int Ano;
+
+    Data() {
+        Dia = 1;
+        Mes = 1;
+        Ano = 1970;
+    }
+
+    Data(int diaInicial, int mesInicial, int anoIncial) {
+        Dia = diaInicial;
+        Mes = mesInicial;
+        Ano = anoIncial;
+    }
 
     String obterDataFormatada() {
         return String.format("%d/%d/%d", Dia, Mes, Ano);
--- src/Exercise/ClassesDesafio/DataTeste.java
+++ src/Exercise/ClassesDesafio/DataTeste.java
@@ -1,24 +1,19 @@
 package Exercise.ClassesDesafio;
 
-public class DesafioClassees2 {
+public class DataTeste {
     public static void main(String[] args) {
 
-        DesafioClasseDatas2 d1 = new DesafioClasseDatas2();
-        d1.Dia = 24;
-        d1.Mes = 11;
-        d1.Ano = 2023;
+        Data d1 = new Data();
+        d1.Ano = 2021;
 
-
-        var d2 = new DesafioClasseDatas2();
-        d2.Dia = 25;
-        d2.Mes = 11;
-        d2.Ano = 2023;
+        var d2 = new Data(31, 12, 2020);
 
         String obterDataFormatada1 = d1.obterDataFormatada();
         System.out.println(obterDataFormatada1);
         System.out.println(d2.obterDataFormatada());
 
         d1.imprimirDataFormatada();
+        d2.imprimirDataFormatada();
 
     }
 }
