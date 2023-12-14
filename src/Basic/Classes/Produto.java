759ebc4e0ac92185565dcc78aea2fb565ac6812c
dan-tovytch <4464640@alunouninter.com>
2023-12-14 14:47:13 -0300

    Update

--- src/Basic/Classes/Produto.java
+++ src/Basic/Classes/Produto.java
@@ -4,16 +4,15 @@ public class Produto {
 
     String nome;
     double preco;
-    double desconto;
+    static double desconto = 0.25;
 
     Produto() {
 
     }
 
-    Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+    Produto(String nomeInicial, double precoInicial) {
         nome = nomeInicial;
         preco = precoInicial;
-        desconto = descontoInicial;
     }
 
     double precoComDesconto() {
--- src/Basic/Classes/ProdutoTeste.java
+++ src/Basic/Classes/ProdutoTeste.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+        Produto p1 = new Produto("Notebook", 4356.89);
 
         var p2 = new Produto();
         p2.nome = "Caneta azul";
         p2.preco = 12.56;
-        p2.desconto = 0.29;
+        Produto.desconto = 0.50;
 
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
+        System.out.println(p1.nome + " " + p1.precoComDesconto());
+        System.out.println(p2.nome + " " + p2.precoComDesconto());
 
         double precoFinal1 = p1.precoComDesconto();
         double precoFinal2 = p2.precoComDesconto(0.1);
