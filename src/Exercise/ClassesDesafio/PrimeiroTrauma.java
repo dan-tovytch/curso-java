de44289db90e05d158c24e192848391449159cc0
dan-tovytch <4464640@alunouninter.com>
2023-12-18 14:56:33 -0300

    Update

---
+++ src/Exercise/ClassesDesafio/PrimeiroTrauma.java
@@ -0,0 +1,13 @@
+package Exercise.ClassesDesafio;
+
+public class PrimeiroTrauma {
+    int a = 3;
+    static int b = 4;
+    public static void main(String[] args) {
+
+        PrimeiroTrauma p = new PrimeiroTrauma();
+        System.out.println(p.a);
+
+        System.out.println(b);
+    }
+}
