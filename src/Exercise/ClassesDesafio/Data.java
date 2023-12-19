993ef75f7743662f9a963cd50a11784e6075b394
dan-tovytch <4464640@alunouninter.com>
2023-12-19 14:56:45 -0300

    Update

--- src/Exercise/ClassesDesafio/Data.java
+++ src/Exercise/ClassesDesafio/Data.java
@@ -7,22 +7,24 @@ public class Data {
     public int Ano;
 
     Data() {
-        Dia = 1;
-        Mes = 1;
-        Ano = 1970;
+//        Dia = 1;
+//        Mes = 1;
+//        Ano = 1970;
+        this(1, 1, 1970);
     }
 
-    public Data(int diaInicial, int mesInicial, int anoIncial) {
-        Dia = diaInicial;
-        Mes = mesInicial;
-        Ano = anoIncial;
+    public Data(int Dia, int Mes, int Ano) {
+        this.Dia = Dia;
+        this.Mes = Mes;
+        this.Ano = Ano;
     }
 
     public String obterDataFormatada() {
-        return String.format("%d/%d/%d", Dia, Mes, Ano);
+        return String.format("%d/%d/%d", this.Dia, Mes, Ano);
     }
 
     void imprimirDataFormatada() {
-        System.out.println(obterDataFormatada());
+        System.out.println(this.obterDataFormatada());
     }
+
 }
