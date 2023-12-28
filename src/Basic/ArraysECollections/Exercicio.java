ad0f96288bd63d2864aae828585af45ea989b191
dan-tovytch <4464640@alunouninter.com>
2023-12-28 17:11:47 -0300

    Update

---
+++ src/Basic/ArraysECollections/Exercicio.java
@@ -0,0 +1,35 @@
+package Basic.ArraysECollections;
+
+import java.util.Arrays;
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+        double[] notasAlunoA = new double[4];
+        notasAlunoA[0] = 7.9;
+        notasAlunoA[1] = 8;
+        notasAlunoA[2] = 6.7;
+        notasAlunoA[3] = 9.7;
+
+        System.out.println(Arrays.toString(notasAlunoA));
+        System.out.println(notasAlunoA[0]);
+        System.out.println(notasAlunoA[notasAlunoA.length - 1]);
+
+        double totalAlunoA = 0;
+        for (int i = 0; i < notasAlunoA.length; i++) {
+            totalAlunoA += notasAlunoA[i];
+        }
+
+        System.out.println(totalAlunoA / notasAlunoA.length);
+
+        final double notaArmazenada = 5.9;
+        double[] notasAlunoB = { 6.9, 8.9, 5.5, notaArmazenada, 10 };
+
+        double totalAlunoB = 0;
+        for (int i = 0; i < notasAlunoB.length ; i++) {
+            totalAlunoB += notasAlunoB[i];
+        }
+
+        System.out.println(totalAlunoB / notasAlunoB.length);
+    }
+}
