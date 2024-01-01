e5e0f43efffd893b787798a001fc989ed0edeb0d
dan-tovytch <4464640@alunouninter.com>
2024-01-01 19:38:46 -0300

    Update

--- src/Exercise/ArrayECollectionsEx/DesafioNotas.java
+++ src/Exercise/ArrayECollectionsEx/DesafioNotas.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class DesafioNotas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double notaMedia = 0.0;
+        double notaMedia = 0;
         System.out.print("Digite a quantidade de notas: ");
         int Qtdnota = scanner.nextInt();
         double[] notasAlunos = new double[Qtdnota];
