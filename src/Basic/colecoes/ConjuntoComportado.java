5f13873142a0a4e61a044c3ae85dc5a758ef3771
dan-tovytch <4464640@alunouninter.com>
2024-01-07 20:20:08 -0300

    Update

---
+++ src/Basic/colecoes/ConjuntoComportado.java
@@ -0,0 +1,34 @@
+package Basic.colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+    public static void main(String[] args) {
+
+//        Set<String> listaAprovados = new HashSet<>();
+        SortedSet<String> listaAprovados = new TreeSet<>();
+        listaAprovados.add("Ana");
+        listaAprovados.add("Carlos");
+        listaAprovados.add("Luca");
+        listaAprovados.add("Pedro");
+
+        for(String candidato: listaAprovados ) {
+            System.out.println(candidato);
+        }
+
+        Set<Integer> nums = new HashSet<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(120);
+        nums.add(6);
+
+        for (int n: nums) {
+            System.out.println(n);
+        }
+    }
+
+}
+
