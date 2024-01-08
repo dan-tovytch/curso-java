44d6652c73ce3aeb0daa86f29514303ddcf29a78
dan-tovytch <4464640@alunouninter.com>
2024-01-08 02:36:02 -0300

    Update

---
+++ src/Basic/ArraysECollections/colecoes/Hash.java
@@ -0,0 +1,16 @@
+package Basic.ArraysECollections.colecoes;
+
+import java.util.HashSet;
+
+public class Hash {
+    public static void main(String[] args) {
+        HashSet<Usuario> usuarios = new HashSet<>();
+
+        usuarios.add(new Usuario("Pedro"));
+        usuarios.add(new Usuario("Ana"));
+        usuarios.add(new Usuario("Guilherme"));
+
+        boolean resultado = usuarios.contains(new Usuario("Guilherme"));
+        System.out.println(resultado);
+    }
+}
--- src/Basic/ArraysECollections/colecoes/Usuario.java
+++ src/Basic/ArraysECollections/colecoes/Usuario.java
@@ -24,6 +24,7 @@ public class Usuario {
 
     @Override
     public int hashCode() {
+
         return Objects.hash(nome);
     }
 }
