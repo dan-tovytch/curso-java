7bc066efff3e981ca62f3fcf3180cc9cafa63264
dan-tovytch <4464640@alunouninter.com>
2024-01-08 00:39:09 -0300

    Update

---
+++ src/Basic/colecoes/Usuario.java
@@ -0,0 +1,29 @@
+package Basic.colecoes;
+
+import java.util.Objects;
+
+public class Usuario {
+    String nome;
+
+    Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Meu nome é " + this.nome + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(nome, usuario.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+}
