36ae0db8208e0e4b18d61de9f22a92ce90502623
dan-tovytch <4464640@alunouninter.com>
2024-01-08 01:48:15 -0300

    Update

---
+++ src/Basic/ArraysECollections/colecoes/Pilha.java
@@ -0,0 +1,36 @@
+package Basic.ArraysECollections.colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+    public static void main(String[] args) {
+
+        Deque<String> livros = new ArrayDeque<>();
+
+        livros.add("O Pequeno Príncipe");
+        livros.push("Don Quixote");
+        livros.push("O Hobbit");
+
+        System.out.println(livros.peek());
+        System.out.println(livros.element());
+
+        for (String livro: livros) {
+            System.out.println(livro);
+        }
+
+        System.out.println(livros.pop());
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+//      System.out.println(livros.pop());
+//      System.out.println(livros.remove());
+
+//        livros.size();
+//        livros.clear();
+//        livros.contains();
+//        livros.isEmpty();
+
+
+    }
+}
