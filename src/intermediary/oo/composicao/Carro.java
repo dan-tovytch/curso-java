30b07caded712e9f14a5db63b863c259806983c4
dan-tovytch <4464640@alunouninter.com>
2024-01-09 00:35:51 -0300

    Update

---
+++ src/intermediary/oo/composicao/Carro.java
@@ -0,0 +1,33 @@
+package intermediary.oo.composicao;
+
+public class Carro {
+
+    final Motor motor;
+
+    Carro() {
+        this.motor = new Motor(this);
+    }
+
+    void acelerar() {
+        if (motor.fatorInjecao < 2.6) {
+             motor.fatorInjecao += 0.4;
+        }
+    }
+    void frear () {
+        if (motor.fatorInjecao > 0.5) {
+             motor.fatorInjecao -= 0.4;
+        }
+    }
+
+    void ligar () {
+        motor.ligado = true;
+    }
+
+    void delisgar() {
+        motor.ligado = false;
+    }
+
+    boolean estaLigado () {
+        return motor.ligado;
+    }
+}
---
+++ src/intermediary/oo/composicao/Motor.java
@@ -0,0 +1,19 @@
+package intermediary.oo.composicao;
+
+public class Motor {
+
+    final Carro carro;
+    boolean ligado = false;
+    double fatorInjecao = 1;
+
+    Motor(Carro carro) {
+        this.carro = carro;
+    }
+    int giros() {
+        if (!ligado) {
+            return 0;
+        } else {
+            return (int) Math.round(fatorInjecao * 3000);
+        }
+    }
+}
